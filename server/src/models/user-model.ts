a75427c81c026a7a87080c5e058225cdd1e42f39
Harsh <pandeygautam039@gmail.com>
2026-03-25 16:01:42 +0530

    update server/src/index.ts and user-model.ts

---
+++ server/src/models/user-model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  googleId: {
+    type: String,
+    select: false,
+  },
+});
+
+const UserModel = mongoose.model("user", UserSchema);
+
+export default UserModel;
