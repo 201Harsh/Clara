12c5c91029ca81187062512073425d6063b7c78b
Harsh <pandeygautam039@gmail.com>
2026-03-28 11:31:52 +0530

    update AxiosInstance.ts

--- client/app/config/AxiosInstance.ts
+++ client/app/config/AxiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000";
+const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:4000";
 
 const AxiosInstance = axios.create({
   baseURL: BASE_URL,
