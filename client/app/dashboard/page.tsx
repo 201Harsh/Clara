a907014505c38c978e8c07351e961cb2addab2bc
Harsh <pandeygautam039@gmail.com>
2026-03-28 12:19:16 +0530

    update page.tsx

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import AxiosInstance from "../config/AxiosInstance";
+
 const page = () => {
+  const GetAllMeetings = async () => {
+    try {
+      const response = await AxiosInstance.get("/calendar/all/meetings");
+
+      console.log(response.data);
+
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    GetAllMeetings();
+  }, []);
+
   return (
     <div className="min-h-screen bg-black text-zinc-100 font-sans flex items-center justify-center relative overflow-hidden p-4">
       <h1 className="text-6xl font-bold">Dashboard Page</h1>
