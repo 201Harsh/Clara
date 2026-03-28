0e4d788d49b6a2d7a2a5c753602fd31191615952
Harsh <pandeygautam039@gmail.com>
2026-03-28 11:33:08 +0530

    update page.tsx

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="min-h-screen bg-black text-zinc-100 font-sans flex items-center justify-center relative overflow-hidden p-4">
+      <h1 className="text-6xl font-bold">Dashboard Page</h1>
+    </div>
+  );
+};
 
-export default page
+export default page;
