e29432b1ffbfc43891a48a231af2dabb53acaa9b
Harsh <pandeygautam039@gmail.com>
2026-03-29 12:42:58 +0530

    update sse.service.ts

--- server/src/services/sse.service.ts
+++ server/src/services/sse.service.ts
@@ -1,6 +1,5 @@
 import { Response } from "express";
 
-// Stores active connections mapped by userId
 const clients = new Map<string, Response>();
 
 export const addSseClient = (userId: string, res: Response) => {
@@ -13,7 +12,6 @@ export const removeSseClient = (userId: string) => {
   console.log(`[SSE] Clara Uplink disconnected for user: ${userId}`);
 };
 
-// Use this function anywhere in your backend to shout to the frontend!
 export const broadcastToUser = (
   userId: string,
   eventType: string,
