d1bbfb0c34dd39d12bb60392a292d9172a884b59
Harsh <pandeygautam039@gmail.com>
2026-03-29 13:08:55 +0530

    chore: delete sse.controller.ts and sse.route.ts

--- server/src/routers/sse.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { establishSseStream } from "../controllers/sse.controller.js";
-
-const sseRouter = Router();
-
-sseRouter.get("/stream", establishSseStream);
-
-export default sseRouter;
