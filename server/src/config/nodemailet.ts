9ac9f122274370aa2891d160e873d4287b9b47e1
Harsh <pandeygautam039@gmail.com>
2026-03-29 12:14:44 +0530

    update nodemailet.ts

--- server/src/config/nodemailet.ts
+++ server/src/config/nodemailet.ts
@@ -1,10 +1,9 @@
 const nodemailer = require("nodemailer");
 
-// Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
   host: "smtp.example.com",
   port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
